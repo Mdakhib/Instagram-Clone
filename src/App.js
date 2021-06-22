abea10523588877625d466f607e95be9ef3a3e7f
Mdakhib <mohamedakhib@gmail.com>
2021-06-22 13:41:17 +0530

    instgram logo added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt=""
+        />
+      </div>
+
+      
+
+
     </div>
   );
 }
