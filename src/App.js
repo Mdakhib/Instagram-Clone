c038f0e19be2eb2ebd1100efc1fc2442b745c973
Mdakhib <mohamedakhib@gmail.com>
2021-06-22 15:05:44 +0530

    data is added in obj and passed through mapping  and then to Post by using props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 import { Post } from "./components";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "a_k_h_i_b",
+      caption: "WoW it's works",
+      imageUrl: "/images/mda.jpg",
+    },
+    {
+      username: "its_yahiya_khan",
+      caption: "chaleyae shuru kartae hai",
+      imageUrl: "/images/yah.jpg",
+    },
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -12,11 +27,26 @@ function App() {
         />
       </div>
 
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+      ))}
+
+      
+      {/* <Post
+        username="saqlain"
+        caption="kaisae hai sab log"
+        imageUrl="/images/saq.jpg"
+      />
+      
+      <Post
+        username="zain"
+        caption="kya mama"
+        imageUrl="/images/zai.jpg" />
+      <Post
+        username="3_idiots"
+        caption="3-bandar"
+        imageUrl="/images/3idiot.jpg"
+      /> */}
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/post.css";
 import { Avatar } from "@material-ui/core";
 
-function Post() {
+function Post({imageUrl,username,caption}) {
   return (
     <div className="post">
       <div className="post__header">
@@ -10,19 +10,19 @@ function Post() {
         <Avatar
           className="post__avatar"
           alt="Akhib"
-          src="/images/profile.jpg"
+          src="/images/profiles.jpg"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
 
       <img
         className="post__image"
-        src="https://res.cloudinary.com/practicaldev/image/fetch/s--50wZvNu6--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/1wwdyw5de8avrdkgtz5n.png"
+        src={imageUrl}
         alt=""
       />
       {/* image */}
       <h4 className="post__text">
-        <strong>a_k_h_i_b</strong> let's build Instagram clone{" "}
+        <strong>{username} </strong> {caption}
       </h4>
       {/* username + caption */}
     </div>
